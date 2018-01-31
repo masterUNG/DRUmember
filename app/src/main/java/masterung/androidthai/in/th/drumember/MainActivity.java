0ed69d7f29e173926602320da39f111157c4c1ce
MasterUNG <phrombutr@gmail.com>
2018-01-31 16:17:03 +0700

    Finish 31Jan

--- app/src/main/java/masterung/androidthai/in/th/drumember/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/drumember/MainActivity.java
@@ -3,11 +3,26 @@ package masterung.androidthai.in.th.drumember;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.drumember.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null) {
+
+//            Add Fragment to Activity
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+
     }
 }
